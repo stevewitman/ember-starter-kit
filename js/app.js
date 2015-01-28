53606a6c59faff318c3b91c858d5dc98080f65f4
Steve Witman <stevewitman@gmail.com>
2015-01-27 22:03:36 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+App = Ember.Application.create();
+App.model = ['one', 'two', 'three']
+App.name = "Tom Waits";
+// App.Router.map(function() {
+//   // put your routes here
+// });
+//
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue', 'green', 'purple', 'brown'];
+//   }
+// });
